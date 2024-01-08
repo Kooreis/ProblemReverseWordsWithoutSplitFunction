d890f5857284594be7dcd1fac02c3c142ded0f27
Kooreis <kooreis.official@gmail.com>
2024-01-08 13:13:07 -0600

    In this update, a for loop is added to iterate over each character in the sentence. If the current character is a space, the current word is added to the 'words' array and 'currentWord' is reset. If the current character is not a space, it is added to 'currentWord'.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,8 @@
-function reverseWordsInSentence(sentence) {
-    let words = [];
-    let currentWord = '';
-}
+for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] === ' ') {
+            words.push(currentWord);
+            currentWord = '';
+        } else {
+            currentWord += sentence[i];
+        }
+    }
